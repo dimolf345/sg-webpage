05e84aa73231443c3e7d9102bdf348c22d1571f6
dimolf345 <dimolfetta.ta@gmail.com>
2022-01-28 17:34:39 +0100

    landing section added with image, added aos library for animation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 require("bootstrap");
+require("aos/dist/aos.css");
+const AOS = require("aos");
+
+// Starting Animate On Scroll (AOS)
+AOS.init({
+  delay: 200,
+  duration: 1000,
+  once: false,
+  mirror: false,
+});
