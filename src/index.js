190b2e3e8329cab8a17f35fcd517fec2781f9313
dimolf345 <dimolfetta.ta@gmail.com>
2022-02-08 22:22:02 +0100

    landing page modified

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+"use strict";
 require("bootstrap");
 require("@fortawesome/fontawesome-free");
 const AOS = require("aos");
@@ -10,7 +11,22 @@ AOS.init({
 });
 
 let navbar = document.querySelector("#topNav");
+let mobileNav = document.querySelector("#mobileNav");
+let toggler = document.querySelector(".navbar-toggler");
+let mobileNavLinks = document.querySelectorAll("#mobileNav .nav-link");
 
+// ===========Automatic Navbar Close on link click=======//
+function handleNavClose() {
+  toggler.click();
+}
+
+mobileNavLinks.forEach((link) => {
+  if (link.href.includes("#")) {
+    link.addEventListener("click", handleNavClose);
+  }
+});
+
+// ===========Navbar shadow when scrolled more than 300px=========//
 function toggleNavbarShadow() {
   if (this.scrollY >= 300) {
     navbar.classList.add("shadow");
